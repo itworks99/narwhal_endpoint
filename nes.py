43b7bba67c06009037f0e40556271518538741ab
Kon <KVanyushov@gmail.com>
2019-06-28 18:56:07 +1000

    fixed cache processing

--- nes.py
+++ nes.py
@@ -16,7 +16,7 @@ DASH_LINE = "-----------------------------------------------------------------"
 configuration = {
     "TRANSMISSION_INTERVAL": 1,
     "COMPRESSION_TYPE": 5,
-    "REMOTE_REDIS_HOST": "",
+    "REMOTE_REDIS_HOST": "192.168.1.14",
     "REMOTE_REDIS_PORT": 6379,
     "REMOTE_REDIS_DB": 1,
     "LOCAL_HOST": "0.0.0.0",
@@ -25,11 +25,14 @@ configuration = {
 }
 
 one_second = 1000000000  # 1 sec = 1000000000 nanoseconds
+send_data_interval = configuration["TRANSMISSION_INTERVAL"] * \
+    one_second
 
 beginning_of_time_interval = ""
 data_block_template = {"dt": [], "ip": [], "endpoint": [], "raw_message": []}
 data_increment = 0
 endpoint_name = ""
+data_block = data_block_template
 
 
 def load_env_variable(env_variable):
@@ -75,59 +78,53 @@ class SyslogHandler(socketserver.BaseRequestHandler):
         global data_block_template
         global endpoint_name
         global configuration
+        global data_block
+
+        raw_message_data = ""
 
         if beginning_of_time_interval == "":
             beginning_of_time_interval = time.monotonic_ns()
 
-        data_block = data_block_template
         raw_message_data = bytes.decode(self.request[0].strip())
         socket = self.request[1]
         dt = datetime.now().isoformat(timespec='microseconds')
 
         time_passed = (time.monotonic_ns() - beginning_of_time_interval)
+
         if time_passed < send_data_interval:
-            if str(raw_message_data).strip() != "":
-                data_block["dt"].append(dt)
-                data_block["ip"].append(self.client_address[0])
-                data_block["raw_message"].append(str(raw_message_data))
-                data_block["endpoint"].append(endpoint_name)
-                raw_message_data = ""
 
-        else:
-            if len(data_block['dt']) > 0:
-                packed_message = msgpack.packb([data_block], use_bin_type=True)
+            data_block["dt"].append(dt)
+            data_block["ip"].append(self.client_address[0])
+            data_block["raw_message"].append(str(raw_message_data))
+            data_block["endpoint"].append(endpoint_name)
+            raw_message_data = ""
 
-                compressed_message = zlib.compress(
-                    packed_message, configuration["COMPRESSION_TYPE"])
+        if time_passed > send_data_interval:
+            packed_message = msgpack.packb([data_block], use_bin_type=True)
 
-                r.rpush('raw_message_block', compressed_message)
-
-                beginning_of_time_interval = time.monotonic_ns()
-                data_block["dt"] = []
-                data_block["ip"] = []
-                data_block["raw_message"] = []
-                data_block["endpoint"] = []
+            compressed_message = zlib.compress(
+                packed_message, configuration["COMPRESSION_TYPE"])
+            r.rpush('raw_message_block', compressed_message)
+            beginning_of_time_interval = time.monotonic_ns()
+            data_block["dt"].clear()
+            data_block["ip"].clear()
+            data_block["raw_message"].clear()
+            data_block["endpoint"].clear()
 
 
 class ThreadingUDPServer(ThreadingMixIn, UDPServer):
-    daemon_threads = True
-    allow_reuse_address = True
     pass
 
 
 class ThreadingTCPServer(ThreadingMixIn, TCPServer):
-    daemon_threads = True
-    allow_reuse_address = True
     pass
 
 
 if __name__ == "__main__":
     try:
-        global send_data_interval
 
         load_configuration()
-        send_data_interval = configuration["TRANSMISSION_INTERVAL"] * \
-            one_second
+
         local_host = configuration["LOCAL_HOST"]
         endpoint_name = socket.getfqdn()
         print(DASH_LINE)
